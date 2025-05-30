10d76f5129cc713df5e11937a32efcef57a9c682
ForgeStove <ForgeStove@outlook.com>
2025-05-31 01:11:50 +0800

    更新项目环境配置

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,4 +4,4 @@ pluginManagement {
 		gradlePluginPortal()
 	}
 }
-plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0" }
+plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "+" }
